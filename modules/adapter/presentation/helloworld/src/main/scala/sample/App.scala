1e6cf950751a5493b63414b9721aa619eefdfd2a
西 悠之 <hiroyuki.nishi@motex.co.jp>
2020-08-22 12:14:46 +0900

    fix

--- modules/adapter/presentation/helloworld/src/main/scala/sample/App.scala
+++ modules/adapter/presentation/helloworld/src/main/scala/sample/App.scala
@@ -9,7 +9,6 @@ import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.sqs.SqsClient
 
 class App extends RequestHandler[util.LinkedHashMap[String, Object], Unit] {
-  // TODO: nishi Regionの環境変数を追加
   private val env: String = sys.env.getOrElse("env", "local")
   private val region: Region =
     Region.of(sys.env.getOrElse("region", Region.AP_NORTHEAST_1.toString))
@@ -22,9 +21,6 @@ class App extends RequestHandler[util.LinkedHashMap[String, Object], Unit] {
   private val signApplication = new SignApplication {
     override protected val signService: SignService = new SignService with SignRepositoryOnSQS {
       override protected val sqsClient: SqsClient = client
-// TODO: nishi
-//        if (env == "local")
-//          sqsClientBuilder.endpointOverride(new URI("http://localhost:4576")).build()
       override protected val queueName: String = "test-sign-local"
     }
   }
